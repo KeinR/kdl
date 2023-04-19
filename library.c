bd149a8641dc02541e93306ae1eb84394de7fe29
Orion Musselman <orionmm@pm.me>
2023-04-18 21:01:44 -0400

    Remove silly example code

--- library.c
+++ library.c
@@ -585,35 +585,5 @@ int main(int argc, char **argv) {
     psd_cleanup();
     */
 
-
-    regex_t regex;
-    int reti;
-    char msgbuf[100];
-
-    /* Compile regular expression */
-    reti = regcomp(&regex, "\\(t[a-z]+)", REG_EXTENDED);
-    if (reti) {
-        fprintf(stderr, "Could not compile regex\n");
-        exit(1);
-    }
-
-    /* Execute regular expression */
-    reti = regexec(&regex, "foobar test", 0, NULL, 0);
-    if (!reti) {
-        puts("Match");
-    }
-    else if (reti == REG_NOMATCH) {
-        puts("No match");
-    }
-    else {
-        regerror(reti, &regex, msgbuf, sizeof(msgbuf));
-        fprintf(stderr, "Regex match failed: %s\n", msgbuf);
-        exit(1);
-    }
-
-    /* Free memory allocated to the pattern buffer by regcomp() */
-    regfree(&regex);
-
-
     return 0;
 }
